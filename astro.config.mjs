9f18396f214f6e44ebf0c4d6e2f774260bbc77ec
Dominik Tichy <tichydominik451@gmail.com>
2026-01-19 22:29:25 +0100

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://dominikandrewtichy.github.io",
+  base: "/final-exam-notes",
+  integrations: [mdx()],
+});
